77dd575ce836e80a1e003a38e45c85f7b11452c7
Eduardo Alves <eduardo.santos.alves14@gmail.com>
2019-02-03 16:46:22 -0200

    Atualização Geral

--- cadastrar.php
+++ cadastrar.php
@@ -5,24 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Cadastro de novo usuário</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="./CSS/estilo.css" />
-    <script src="main.js"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="./sytileCadastro.css" />
+    
 </head>
 <body>
 
-    <div id="corpo-form">
-        <img src="./CSS/IMAGE/logo-preto.png.png" alt="Logo da igreja Evangélica Avivamento Biblíco" width="250px">
+    <div id="div-corp-cadastro">
+        <img src="./IMAGE/logoIEAB.png" alt="Logo da igreja Evangélica Avivamento Biblíco" width="250px">
         
-        <h3>Cadastrar Novo Usuário</h3>
 
-        <form method = "POST" action="processa.php">
+        <form id="form-cadastro" method = "POST" action="processa.php">
 
-             <input type="text" placeholder="Nome completo">
+             <input type="email" placeholder="Seu E-mail">
              <input type="text" placeholder="Telefone">
              <input type="email" placeholder="Usuário">
              <input type="password" placeholder="Senha">
              <input type="password" placeholder="Confirmar Senha">
-             <input type="submit" value="CADASTRAR">
+             <input id="input-cadastrar" type="submit" value="CADASTRAR">
              
             
         </form>
--- index.php
+++ index.php
@@ -5,11 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="./CSS/style.css" />
-    <script src="main.js"></script>
+    <link rel="stylesheet" type="text/css" href="./style.css">
+  
 </head>
 <body>
-
+  
     <div id="form-div">
       <strong><samp id="span-top">Igreja Evangélica Avivamento Biblíco</samp></strong>
       
@@ -17,12 +17,13 @@
        
         <hr>
 
-        <img src="./CSS/IMAGE/logoIEAB.png" alt="">
+       <img src="./IMAGE/logoIEAB.png" alt="Logo da igrja evangélica  avivamento biblíco">
         
         <input type="email" placeholder="Seu nome de usuário">
         <input type="password" placeholder="Sua senha de cesso">
         <input id="input-acess" type="submit" value="Entrar">
-        <p>Cadastrar novo adimistrador <a href="cadastrar.php">Cadastra-se</a></p> 
+        <p>Cadastrar novo adiministrador.<strong><a href="cadastrar.php"> Clik aqui!</a></strong>  </p> 
+        
         
       
       </form>
